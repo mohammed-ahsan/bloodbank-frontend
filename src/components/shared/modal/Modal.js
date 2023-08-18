5d3f675592f55f7bc11fe5ce8139f21af1603ddb
Mohammed Ahsan <earth.ahsan@gmail.com>
2023-08-18 08:06:08 +0600

    fixes

--- src/components/shared/modal/Modal.js
+++ src/components/shared/modal/Modal.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import InputType from "./../Form/InputType";
 import API from "./../../../services/API";
-import { Button,Modal as AntMod,Dropdown,Radio,Input } from 'antd';
+import { Button,Modal as AntMod,Dropdown,Radio,Input,AutoComplete } from 'antd';
 import { useEffect } from "react";
 
 const Modal = ({isModal,handleCancel,showModal,handleOk}) => {
@@ -11,7 +11,71 @@ const Modal = ({isModal,handleCancel,showModal,handleOk}) => {
   const [quantity, setQuantity] = useState(0);
   const [email, setEmail] = useState("");
   const [divison,setDivison] = useState("")
-  const [district,setDistrict] = useState("")
+  const [district,setDistrict] = useState()
+
+  const options = [
+    { value: 'Rajshahi'},
+    { value: 'Dhaka' },
+    { value: 'Barisal' },
+    { value: 'Khulna' },
+    { value: 'Chittagong' },
+    { value: 'Sylhet' },
+    { value: 'Rangpur' },
+    { value: 'Mymensingh' },
+
+  ];
+  const optionDist = [
+     [{ value:"Barguna"} ,  { value:"Barisal"} ,        { value:"Bhola"},   { value: "Jhalokati"},  { value:"Patuakhali"}, { value:"Pirojpur"}],
+    [{ value:"Bandarban"} ,{ value:"Brahmanbaria"} ,  { value: "Chandpur"}, { value:"Chittagong"},{ value: "Comilla"},   { value: "Cox's Bazar"},{ value:"Feni"},     { value:"Khagrachhari"},{ value:"Lakshmipur"},{ value: "Noakhali"}, { value:"Rangamati"}],
+      [{ value:"Dhaka"} ,    { value:"Faridpur"} ,      { value: "Gazipur"}, { value: "Gopalganj"}, { value: "Kishoreganj"},{ value:"Madaripur"}, { value: "Manikganj"},{ value:"Munshiganj"},  { value:"Narayanganj"},{ value:"Narsingdi"},{ value:"Rajbari"},{ value:"Shariatpur"},{ value:"Tangail"}],
+     [{ value:"Bagerhat"} , { value:"Chuadanga"} ,    { value:  "Jessore"}, { value: "Jhenaidah"}, { value: "Khulna"},    { value: "Kushtia"},    { value:"Magura"},  { value: "Meherpur"},   { value: "Narail"},   { value:  "Satkhira"}],
+   [{ value:"Jamalpur"} , { value: "Mymensingh"} ,     { value:"Netrakona"},{ value:"Sherpur"}],
+      [{ value:"Bogra"},    { value:"Chapainawabganj"},{ value:"Joypurhat"},{ value:"Naogaon"},    { value:"Natore"},     { value:"Pabna"},      { value:"Rajshahi"}, { value:"Sirajganj"}],
+      [{ value:"Dinajpur"} , { value:"Gaibandha"} ,      { value:"Kurigram"}, { value:"Lalmonirhat"},{ value:"Nilphamari"}, { value:"Panchagarh"}, { value:"Rangpur"},  { value:"Thakurgaon"}],
+       [{ value:"Habiganj"} , { value:"Moulvibazar"} ,   { value: "Sunamganj"},{ value:"Sylhet"}]
+  ]
+  
+  
+  const onSelect = (data) => {
+    console.log(optionDist)
+    
+    setDivison(data)
+   
+    
+   
+  
+    
+   
+    
+  };
+  useEffect(()=>{
+    
+    if(divison == "Rajshahi")
+    {
+      setDistrict(5)
+    }
+    else if(divison == "Barisal"){
+setDistrict(0)
+    }
+    else if(divison == "Chittagong"){
+      setDistrict(1)
+          }
+          else if(divison == "Dhaka"){
+            setDistrict(2)
+                }
+                else if(divison == "Khulna"){
+                  setDistrict(3)
+                      }
+                      else if(divison == "Mymensingh"){
+                        setDistrict(4)
+                            }
+                            else if(divison == "Rangpur"){
+                              setDistrict(6)
+                                  }
+                                  else if(divison == "Sylhet"){
+                                    setDistrict(7)
+                                        }
+  },[divison])
   const { user } = useSelector((state) => state.auth);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const items = [{
@@ -55,6 +119,7 @@ const Modal = ({isModal,handleCancel,showModal,handleOk}) => {
 
 
 ]
+
 const [bloodGroup, setBloodGroup] = useState(items[0].label);
   const handleModalSubmit = async () => {
     try {
@@ -161,22 +226,33 @@ const [bloodGroup, setBloodGroup] = useState(items[0].label);
                 value={quantity}
                 onChange={(e) => setQuantity(e.target.value)}
               />
-              Divison
-            <Input
-                
-                placeholder="divison"
-                inputType={"text"}
-                value={divison}
-                onChange={(e) => setDivison(e.target.value)}
-              />
-              District
-              <Input
-                
-                placeholder="district"
-                inputType={"text"}
-                value={district}
-                onChange={(e) => setDistrict(e.target.value)}
-              />
+            <div>
+                  Division :  {"  "}
+                   <AutoComplete
+                   
+        options={options}
+        style={{ width: 200 }}
+        onSelect={onSelect}
+        filterOption={(inputValue, option) =>
+          option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+        placeholder="Division"
+      />
+                  </div>
+                  
+                 {divison.length>0?<div>
+                  District :  {"  "}
+                   <AutoComplete
+                   
+        options={optionDist[district]}
+        style={{ width: 200 }}
+        onSelect={onSelect}
+        filterOption={(inputValue, option) =>
+          option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+        placeholder="District"
+      />
+                  </div>:<p>Select Divison First!</p> } 
            
           </div>
         </div>
